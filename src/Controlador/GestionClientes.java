7648e17b25e1a3326e9edbbe236f2e9e4eaaee50
jf3palacios <jf3.palacios@gmail.com>
2023-10-07 17:32:28 +0200

    first version project

---
+++ src/Controlador/GestionClientes.java
@@ -0,0 +1,43 @@
+package Controlador;
+
+public class GestionClientes {
+
+    /**
+     * Constructor
+     */
+    public GestionClientes() {
+    }
+
+    /**
+     * Metodo Agregar Cliente
+     */
+    public void addClientes() {
+
+    }
+
+    /**
+     * Metodo Ver Cliente
+     */
+    public void showClientes(){
+
+    }
+
+    /**
+     * Metodo Ver Clientes Stantard
+     */
+    public void showClientesStandard(){
+
+    }
+
+    /**
+     * Metodo Ver Clientes Premium
+     */
+    public void showClientesPremium(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "GestionClientes{}";
+    }
+}
